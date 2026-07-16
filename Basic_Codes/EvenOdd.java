f3b33b0e43a8ab2b097dacb02473040ecd8473f2
dnyaneshwari_varal <varaldnyaneshwari@gmail.com>
2026-07-16 13:02:45 +0530

    Day1_DSA

---
+++ Basic_Codes/EvenOdd.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+public class EvenOdd{
+    public int evenOdd(int x){
+        if(x%2==0){
+            return 0;
+        }
+        return 1;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        EvenOdd obj=new EvenOdd();
+
+        int x=sc.nextInt();
+        int res=obj.evenOdd(x);
+
+        if(res==1){
+            System.out.println(x +"is odd");
+        }
+        else{
+            System.out.println(x +"is even");
+        }
+            
+    }
+}
