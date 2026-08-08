0a6a27a6a480ae3b277cd9fc7a48899ed4169e9d
dnyaneshwari_varal <varaldnyaneshwari@gmail.com>
2026-08-08 15:25:38 +0530

    rotateToLeftByK

---
+++ Striver_Sheet/Array/LeftRotateByK.java
@@ -0,0 +1,41 @@
+package Striver_Sheet.Array;
+
+public class LeftRotateByK {
+	
+	public static void reverse(int[] arr,int start,int end) {
+		while(start<end) {
+			int temp=arr[start];
+			arr[start]=arr[end];
+			arr[end]=temp;
+			
+			start++;
+			end--;
+		}
+	}
+
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+		int arr[]= {1,2,3,4,5};
+		int k=2;
+		System.out.print("Original ");
+		System.out.println();
+
+		for(int i=0;i<arr.length;i++) {
+						System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+		reverse(arr,0,k-1);
+		reverse(arr,k,arr.length-1);
+		reverse(arr,0,arr.length-1);
+		
+		System.out.println("rotated to left by K="+k);
+		
+		for(int i=0;i<arr.length;i++) {
+			
+			System.out.print(arr[i]+" ");
+			
+		}
+
+	}
+
+}
