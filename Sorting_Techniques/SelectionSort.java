10821ffd7b4375bc5cce298c4dd52ba267181384
dnyaneshwari_varal <varaldnyaneshwari@gmail.com>
2026-08-17 16:45:39 +0530

    SelectionSort

---
+++ Sorting_Techniques/SelectionSort.java
@@ -0,0 +1,30 @@
+package Sorting_Techniques;
+
+public class SelectionSort {
+
+	public static void selectionSort(int[] arr) {
+		
+		
+		for(int i=0;i<arr.length;i++) {
+			int min=i;
+			for(int j=i+1;j<arr.length;j++) {
+				if(arr[j]<arr[min]) {
+					min=j;
+				}
+			}
+			int temp=arr[i];
+			arr[i]=arr[min];
+			arr[min]=temp;
+		}
+		
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+	public static void main(String[] args) {
+		int arr[]= {66,12,54,65,23,45};
+		
+		selectionSort(arr);
+	}
+
+}
